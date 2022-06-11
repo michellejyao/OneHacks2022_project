b4573af5f7c39eac00cf7e6f13090391680f765d
chalory <chantalrain.pino@gmail.com>
2022-06-12 01:17:01 +0800

    boilerplate

--- onehacks-backend/db.py
+++ onehacks-backend/db.py
@@ -15,12 +15,12 @@ import logging
 import psycopg2
 from psycopg2.errors import SerializationFailure
 
-conn = psycopg2.connect("postgresql://chantal:onehacks2022@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Donehacks-2022-2773")
+conn = psycopg2.connect("postgresql://chantal:onehacks2022-environment@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Donehackss-2022-2774")
 
 def info(conn):
     with conn.cursor() as cur:
         cur.execute(
-             "CREATE TABLE IF NOT EXISTS user_info (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), email VARCHAR, first_name VARCHAR, last_name VARCHAR, contact_number VARCHAR(15))")
+             "CREATE TABLE IF NOT EXISTS user_info (email VARCHAR, password VARCHAR)")
       
         logging.debug("create_accounts(): status message: %s",
                       cur.statusmessage)
@@ -29,13 +29,4 @@ def info(conn):
    
     conn.commit()  
 info(conn)
-
-def insert_activity(conn, info):
-    with conn.cursor() as cur:
-        cur.execute(
-            f"UPSERT INTO activities (email, first_name, last_name, contact_number) VALUES ('{info['email']}','{info['first_name']}','{info['last_name']}',{info['contact_number']}')"
-        )
-        logging.debug("init_db(): status message: %s",
-                      cur.statusmessage)
-    conn.commit()
 
