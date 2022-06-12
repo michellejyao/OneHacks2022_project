0ac978df8f2372b94f9c706ec63a6b24e18499ac
chalory <chantalrain.pino@gmail.com>
2022-06-12 08:40:30 +0800

    insert

--- onehacks-backend/db.py
+++ onehacks-backend/db.py
@@ -15,7 +15,7 @@ import logging
 import psycopg2
 from psycopg2.errors import SerializationFailure
 
-conn = psycopg2.connect("postgresql://chantal:onehacks2022-environment@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Donehackss-2022-2774")
+conn = psycopg2.connect("postgresql://chantal:onehacks2022@free-tier6.gcp-asia-southeast1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Donehacks-backend-2776")
 
 def info(conn):
     with conn.cursor() as cur:
@@ -26,7 +26,17 @@ def info(conn):
                       cur.statusmessage)
        
     
-   
     conn.commit()  
+
+
 info(conn)
+
+def insert_activity(conn, email, password):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"UPSERT INTO activities (email, password) VALUES ({email}, {password})"
+        )
+        logging.debug("init_db(): status message: %s",
+                      cur.statusmessage)
+    conn.commit()
 
